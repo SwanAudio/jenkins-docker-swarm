b5c0adba89d1688b80022dbfd9bb02273c31bb52
Evan Lowry <lowry.e@gmail.com>
2018-01-14 07:19:19 -0400

    Fix fsroot path
    Workspace was being double applied to the path.

--- start-slave.sh
+++ start-slave.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-java -jar swarm-client.jar -fsroot /home/jenkins/workspace -executors 2 -master http://jenkins-master:8080 -username $(cat /run/secrets/jenkins-username) -password $(cat /run/secrets/jenkins-password)
+java -jar swarm-client.jar -fsroot /home/jenkins -executors 2 -master http://jenkins-master:8080 -username $(cat /run/secrets/jenkins-username) -password $(cat /run/secrets/jenkins-password)
